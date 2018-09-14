988ba334487b10bd16c0f03304d21575aaad7fbb
T. Carl Beery <carlbeery@outlook.com>
2018-09-13 19:43:10 -0700

    Remove gui components

--- main.cpp
+++ main.cpp
@@ -4,16 +4,5 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-    QGuiApplication app(argc, argv);
-
     qDebug() << "Hello World";
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-
-    return app.exec();
 }
